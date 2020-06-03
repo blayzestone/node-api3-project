3978a4bbf7c102ede5639e42d44d7cbccc6bce29
Blayze <blayzestone@protonmail.com>
2020-06-03 12:56:26 -0700

    create logger custom middleware

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // code away!
+const server = require("./server");
+
+server.listen(8000, () => console.log("\n server is running on port 8000 \n"));
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.get("/", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `${req.method} ${req.protocol}://${req.hostname}${req.path} ${new Date()}`
+  );
+  next();
+}
 
 module.exports = server;
